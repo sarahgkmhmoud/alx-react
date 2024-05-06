693c71b20ec97dedfd26cb8d0b923d8a47c2d030
Sarahgad <sarahgad91@gmail.com>
2024-05-07 00:18:18 +0300

    try html

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,27 +1,19 @@
 import $ from 'jquery';
-import debounce from 'lodash/debounce';
+
+import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<Button>Click here to get started</Button>');
+$('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-function updateCounter() {
-    // Get the paragraph element with id='count'
-    const countParagraph = document.getElementById('count');
-    
-    // Initialize counter if not exists
-    if (!updateCounter.counter) {
-        updateCounter.counter = 0;
-    }
-    
-    // Increment the counter
-    updateCounter.counter++;
-    
-    // Update the content of the paragraph element
-    countParagraph.textContent = `${updateCounter.counter} clicks on the button`;
+
+let counter = 0;
+
+function updateCounter(){
+	counter ++;
+	$("#count").html('${counter} clicks on the button');
 }
 
-const button = document.getElementById('your-button-id'); // Replace 'your-button-id' with the actual id of your button element
-button.addEventListener('click', debounce(updateCounter, 300)); // Adjust debounce delay (in milliseconds) as needed
+$('button').on('click', _.debounce(updateCounter, 500));
