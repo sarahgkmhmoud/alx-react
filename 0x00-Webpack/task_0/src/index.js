e875c7add00031b4051732be30d5e58d973b918a
Sarahgad <sarahgad91@gmail.com>
2024-05-06 22:44:13 +0300

    edit script

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,19 +1,7 @@
-// Import jQuery
 import $ from 'jquery';
 
-// Function to add paragraphs to the body
-function addParagraphsToBody() {
-    // Add first paragraph
     $('body').append('<p>Holberton Dashboard</p>');
     
-    // Add second paragraph
     $('body').append('<p>Dashboard data for the students</p>');
 
-    // Add third paragraph
     $('body').append('<p>Copyright - Holberton School</p>');
-}
-
-// Call the function to add paragraphs to the body when the DOM is ready
-$(document).ready(function() {
-    addParagraphsToBody();
-});
