8445e2a7d4e373f46535982bedd3b689df648c7a
Sarahgad <sarahgad91@gmail.com>
2024-05-11 23:17:48 +0300

    install Enzyme

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -1,8 +1,17 @@
 import logo from './HolbertonLogo.jpeg'
 import './App.css';
 import {getFullYear, getFooterCopy} from './utils'
+import { useRef } from 'react'
 
 function App() {
+  const emailInputRef = useRef(null);
+  const passwordInputRef = useRef(null);
+
+
+  const handleLabelClick = (ref) => {
+    ref.current.focus();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +20,13 @@ function App() {
       </header>
       <body className="App-body">
         <p>Login to access the full dashboard</p>
+        <form>
+        <label onClick={() => handleLabelClick(emailInputRef)}>Email:</label>
+        <input id="emailField" ref={emailInputRef} type="email"></input>
+        <label onClick={()=> handleLabelClick(passwordInputRef)}>Password:</label>
+        <input id="passwordField" ref={passwordInputRef} type="password"></input>
+        <button>OK</button>
+        </form>
       </body>
       <footer className="App-footer">
         <p>
